9b692a4ccd838cdabe14357178628b13a9b2fba9
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2022-07-29 01:40:13 +0900

    コードをコンポーネントとして抽出する。prettireの導入。インデントの変更。

---
+++ src/components/CircleButton.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { string } from 'prop-types';
+
+export default function CirclButton(props) {
+  const { children } = props;
+  return (
+    <View style={styles.circlButton}>
+      <Text style={styles.circlButtonLabel}>{children}</Text>
+    </View>
+  );
+}
+
+CirclButton.propTypes = {
+  children: string.isRequired,
+};
+
+const styles = StyleSheet.create({
+  circlButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  circlButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 40,
+  },
+});
