6e9d1f40453d50d4f8f1618833df745f048fba6b
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2022-07-29 16:02:50 +0900

    いろいろなアイコンを表示する

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -18,7 +18,7 @@ export default function MemoDetailScreen() {
           ああああああああああああああああああああああああああああああああ ああああああああ
         </Text>
       </ScrollView>
-      <CircleButton style={{ top: 160, bottom: 'auto' }}>+</CircleButton>
+      <CircleButton style={{ top: 160, bottom: 'auto' }} name="edit-2" />
     </View>
   );
 }
