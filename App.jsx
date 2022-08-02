cac293e2b3b23d6ec2d4a771b5166997911c3f0f
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2022-08-02 16:07:44 +0900

    ログイン画面のスタイリング

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoCreateScreen from './src/screens/MemoCreateScreen';
-import LogInScreen from './src/screens/LoginScreen';
+import LogInScreen from './src/screens/LogInScreen';
 
 export default function App() {
   return <LogInScreen />;
