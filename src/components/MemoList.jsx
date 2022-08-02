d55cc752d4f98f7797fbbc46596d5e096bc71b18
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2022-08-02 17:53:31 +0900

    ユーザーのアクションを受け取る

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from './Icon';
 
 export default function MemoList() {
@@ -10,9 +10,9 @@ export default function MemoList() {
           <Text style={styles.memoListItemTitle}>買い物リスト</Text>
           <Text style={styles.memoListItemDate}>2022年7月29日 10:00</Text>
         </View>
-        <View>
+        <TouchableOpacity>
           <Icon name="delete" size={24} color="#B0B0B0" />
-        </View>
+        </TouchableOpacity>
       </View>
 
       <View style={styles.memoListItem}>
