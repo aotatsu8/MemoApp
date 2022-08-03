27150a68037dcb7e157af70968399b0f3485f562
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2022-08-04 01:03:42 +0900

    ナビゲーションを導入して画面をつなぐ

--- App.jsx
+++ App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -7,6 +9,14 @@ import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import LogInScreen from './src/screens/LogInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 
+const Stack = createStackNavigator();
+
 export default function App() {
-  return <MemoListScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MemoListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
