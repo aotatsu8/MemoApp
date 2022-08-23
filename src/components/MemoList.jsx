80b09f6417ef1da3afd0931a049f3b851e3684e6
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2022-08-23 15:59:32 +0900

    メモの削除を実装する

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { shape, string, instanceOf, arrayOf } from 'prop-types';
-import React from 'react';
+import firebase from 'firebase';
 import { View, Text, StyleSheet, TouchableOpacity, Alert, FlatList } from 'react-native';
 import { dateToString } from '../utils';
 import Icon from './Icon';
@@ -8,6 +9,29 @@ import Icon from './Icon';
 export default function MemoList(props) {
   const { memos } = props;
   const navigation = useNavigation();
+
+  function deleteMemo(id) {
+    const { currentUser } = firebase.auth();
+    if (currentUser) {
+      const db = firebase.firestore();
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+      Alert.alert('メモを削除します', 'よろしいですか？', [
+        {
+          text: 'キャンセル',
+          onPress: () => {},
+        },
+        {
+          text: '削除する',
+          style: 'destructive',
+          onPress: () => {
+            ref.delete().catch(() => {
+              Alert.alert('削除に失敗しました');
+            });
+          },
+        },
+      ]);
+    }
+  }
 
   function renderItem({ item }) {
     return (
@@ -26,7 +50,7 @@ export default function MemoList(props) {
         <TouchableOpacity
           style={styles.memoDelete}
           onPress={() => {
-            Alert.alert('Are you sure?');
+            deleteMemo(item.id);
           }}
         >
           <Icon name="delete" size={24} color="#B0B0B0" />
